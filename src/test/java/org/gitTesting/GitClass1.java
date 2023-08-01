908435bf21b55a9824da20b619b07b7face86347
MuruganMara <muruganmara008@gmail.com>
2023-08-01 22:53:40 +0530

    gittesting1

---
+++ src/test/java/org/gitTesting/GitClass1.java
@@ -0,0 +1,17 @@
+package org.gitTesting;
+
+
+public class GitClass1{
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("1234");
+		System.out.println("ghjkl");
+		System.out.println("hjkl456");
+		System.out.println("678hmbnm234");
+		
+	}
+
+
+}
